4ce86f3a8aaece04fbe2184a2bf976fc041be286
kandkailash <147226706+kandkailash@users.noreply.github.com>
2026-06-29 14:21:02 +0530

    Update download_models.py

--- download_models.py
+++ download_models.py
@@ -3,30 +3,32 @@ from huggingface_hub import hf_hub_download
 
 MODEL_DIR = "models"
 
-os.makedirs(MODEL_DIR, exist_ok=True)
-
 MODELS = [
     "best_attunet_ham10000.pth",
     "best_hybrid_cbm.pth"
 ]
 
-for model in MODELS:
 
-    local_path = os.path.join(MODEL_DIR, model)
+def ensure_models():
 
-    if os.path.exists(local_path):
-        print(f"✓ {model} already exists")
-        continue
+    os.makedirs(MODEL_DIR, exist_ok=True)
 
-    print(f"Downloading {model}...")
+    for model in MODELS:
 
-    hf_hub_download(
-        repo_id="kailashkandpal/CARE-MD-Models",
-        filename=model,
-        local_dir=MODEL_DIR,
-        local_dir_use_symlinks=False
-    )
+        local_path = os.path.join(MODEL_DIR, model)
 
-    print(f"✓ Downloaded {model}")
+        if os.path.exists(local_path):
+            print(f"✓ {model} already exists")
+            continue
 
-print("\nAll models are ready.")
+        print(f"Downloading {model}...")
+
+        hf_hub_download(
+            repo_id="kailashkandpal/CARE-MD-Models",
+            filename=model,
+            local_dir=MODEL_DIR
+        )
+
+        print(f"✓ Downloaded {model}")
+
+    print("✓ All models are ready")
